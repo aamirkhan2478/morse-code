76a6e89da17d528bb3937d38bbfc9e6b6e2bd5a8
aamir <aamir.khan2478@gmail.com>
2022-10-26 15:13:03 +0500

    Fix linters errors

--- morse.rb
+++ morse.rb
@@ -1,52 +1,51 @@
 @dictionary = {
-	'.-' => 'A',
-	'-...' => 'B',
-	'-.-.' => 'C',
-	'-..' => 'D',
-	'.' => 'E',
-	'..-.' => 'F',
-	'--.' => 'G',
-	'....' => 'H',
-	'..' => 'I',
-	'.---' => 'J',
-	'-.-' => 'K',
-	'.-..' => 'L',
-	'--' => 'M',
-	'-.' => 'N',
-	'---' => 'O',
-	'.--.' => 'P',
-	'--.-' => 'Q',
-	'.-.' => 'R',
-	'...' => 'S',
-	'-' => 'T',
-	'..-' => 'U',
-	'...-' => 'V',
-	'.--' => 'W',
-	'-..-' => 'X',
-	'-.--' => 'Y',
-	'--..' => 'Z'
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
 }
 
 def decode_char(letter)
-    @letter = @dictionary[letter]
-    return @letter
+  @letter = @dictionary[letter]
+  @letter
 end
 
 def decode_word(word)
-    @word_array = word.split
-    @current_char = ''
-    @word_array.each do |one_word|
-        @current_char += decode_char(one_word)
-    end
-    print "#{@current_char} "
+  @word_array = word.split
+  @current_char = ''
+  @word_array.each do |one_word|
+    @current_char += decode_char(one_word)
+  end
+  print "#{@current_char} "
 end
 
 def decode_message(message)
-    @message_arr= message.split("   ")
-    @message_arr.each do |msg|
-        decode_word(msg)
-    end
+  @message_arr = message.split('   ')
+  @message_arr.each do |msg|
+    decode_word(msg)
+  end
 end
 
-decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
-
+decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
